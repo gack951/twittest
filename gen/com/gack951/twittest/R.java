9458b8515bb26e6591a6196ec7a898ee2a652848
gack951 <gack951@gmail.com>
2013-01-05 20:51:08 +0900

    copied from web

--- gen/com/gack951/twittest/R.java
+++ gen/com/gack951/twittest/R.java
@@ -14,10 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int login=0x7f070000;
+        public static final int menu_settings=0x7f070002;
+        public static final int tweet=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_index=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int activity_index=0x7f060000;
@@ -26,6 +29,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int tweet_button_text=0x7f040003;
     }
     public static final class style {
         /** 
